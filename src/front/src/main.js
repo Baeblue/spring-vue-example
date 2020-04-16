c3dceb370800242514b9fcb73244a034b0551cf8
Baeblue <seungmin941215@gmail.com>
2020-04-16 16:41:11 +0900

    comment

--- src/front/src/main.js
+++ src/front/src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false;  // false로 설정하면 배포에 대한 팁을 출력하지 않음.
 
 new Vue({
   router,  // inject the router to make whole app router-aware
   //el: '#app',
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#app");  // $mount는 el 속성과 같이 인스턴스를 화면에 붙여주는 역할 수행.
